be05e730bf29d2a1236002ac9970cb8ee72de9e6
BogdanATA <batameev@gmail.com>
2026-04-08 10:43:22 -0400

    finshed problems 4-6. square root of variable - distance between points - absolute value

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -26,7 +26,30 @@ public class MathApp {
         double radius = 7.25;
         double area = Math.PI * Math.pow(radius, 2);
 
-        System.out.println("Area of the circle is: " + area);
-        
+        System.out.println("The area of the circle is: " + area);
+
+
+        // find and display the square root of a variable after it is set to 5.0
+        double initialNum = 5.0;
+        double squareRoot = Math.pow(initialNum, 2);
+
+        System.out.println("The square root is: " + squareRoot);
+
+
+        // find and display the distance between the points
+        int x1 = 5;
+        int y1 = 10;
+        int x2 = 85;
+        int y2 = 50;
+        double pointDistance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("The distance is: " + pointDistance);
+
+
+        //find and display the absolute value of a variable
+        double negativeNum = -3.8;
+        double absoluteValue = Math.abs(negativeNum);
+
+        System.out.println("This is the absolute number: " + absoluteValue);
     }
 }
