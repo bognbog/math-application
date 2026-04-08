7138bd26859d8a225df9f0cfb7e47d6a7847749a
BogdanATA <batameev@gmail.com>
2026-04-08 10:17:36 -0400

    finished the first 3 questions - found highest salary, lowest price, and area of circle

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+
+        // determine who has the highest salary
+        int bobSalary = 55000;
+        int garySalary = 63000;
+        int highestSalary;
+
+        highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is: $" + highestSalary);
+
+
+
+        // determine lowest price of car vs truck
+        int carPrice = 35000;
+        int truckPrice = 42000;
+        int lowestPrice;
+
+        lowestPrice = Math.min(carPrice, truckPrice);
+        System.out.println("The lowest price is: $" + lowestPrice);
+
+
+        //find display area of circle
+        double radius = 7.25;
+        double area = Math.Pi * Math.pow(radius, 2);
+
+        System.out.println("Area of the circle is: " + area);
+    }
+}
